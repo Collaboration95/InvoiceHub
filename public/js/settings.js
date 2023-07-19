58e4102be7f54a85e05d9de765b4bc35b2e8ba07
Guruprasath <guruprasath3200@gmail.com>
2023-07-19 12:12:28 +0800

    cleaned up old js code

--- public/js/settings.js
+++ public/js/settings.js
@@ -74,9 +74,6 @@ function fillInvoices(){
         const options = { timeZone: 'Asia/Singapore' };
         invoice.upload_date = (invoice.upload_date).toLocaleDateString('en-SG', options);
 
-        console.log(invoice.upload_date); 
-
-
         // var deleteIcon = `
         // <svg class="ic_delete"  xmlns="http://www.w3.org/2000/svg" value="${invoice.path}" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
         //     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
@@ -96,8 +93,7 @@ function fillInvoices(){
               </svg>`;
       
           // // render each row of data
-
-          
+    
           tableBody.innerHTML += `
             <tr>
               <td>${invoice.invoiceid}</td>
@@ -106,78 +102,6 @@ function fillInvoices(){
               <td>${invoice.total}</td>
               <td>${previewIcon}  ${exportIcon}</td>
             </tr>`;
-          
-        // var previewIcon = document.createElement("a");
-        // previewIcon.href = "#";
-
-        // var previewSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        // previewSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        // previewSvg.setAttribute("width", "16");
-        // previewSvg.setAttribute("height", "16");
-        // previewSvg.setAttribute("fill", "currentColor");
-        // previewSvg.setAttribute("class", "bi bi-eye");
-        // previewSvg.setAttribute("viewBox", "0 0 16 16");
-
-        // var previewPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        // previewPath.setAttribute(
-        //   "d",
-        //   "M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"
-        // );
-
-        // previewSvg.appendChild(previewPath);
-        // previewIcon.appendChild(previewSvg);
-
-        // // Create the export icon element
-        // var exportIcon = document.createElement("a");
-        // exportIcon.href = "#";
-
-        // var exportSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        // exportSvg.setAttribute("class", "ic_export");
-        // exportSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        // exportSvg.setAttribute("width", "16");
-        // exportSvg.setAttribute("height", "16");
-        // exportSvg.setAttribute("fill", "currentColor");
-        // exportSvg.setAttribute("class", "bi bi-box-arrow-right");
-        // exportSvg.setAttribute("viewBox", "0 0 16 16");
-        
-        // var exportPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        // exportPath.setAttribute("fill-rule", "evenodd");
-        // exportPath.setAttribute(
-        //   "d",
-        //   "M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
-        // );
-
-        // exportSvg.appendChild(exportPath);
-        // exportIcon.appendChild(exportSvg);
-        // exportIcon.value=invoice.path;
-
-        // // Add event listener to the preview icon
-        // previewIcon.addEventListener("click", function (event) {
-        //   console.log("Preview icon clicked");
-        //   // Add your code to handle the preview icon click event
-        // });
-
-        // exportIcon.addEventListener('click',(event)=>{
-        //   const filePath = 'http://127.0.0.1:8080/'+event.target.value;
-        //   //   window.open(filePath);
-        // })
-
-        // // Add the row to the table
-        // table.innerHTML += `
-        //   <tr>
-        //     <td>${invoice.invoiceid}</td>
-        //     <td>${invoice.invoice_name}</td>
-        //     <td>${invoice.upload_date}</td>
-        //     <td>${previewIcon.outerHTML} ${exportIcon.outerHTML}</td>
-        //   </tr>`;
-
-
-
-
-                
-        // row.appendChild(notifs);
-        
-
 
       });
     })
