b94015ad85958da92c339b58cd97d601a41a277b
ChewonHan <about0322@naver.com>
2023-07-22 17:32:03 +0800

    removed invoice table on settingPage

--- public/js/settings.js
+++ public/js/settings.js
@@ -7,7 +7,7 @@ if (deleteAccountButton) {
 
 fillTable();
 
-fillInvoices();
+// fillInvoices();
 // Assuming you have a variable called isAdminMode that indicates whether you're in admin mode or not
 const dangerzoneDiv = document.getElementById('dangerzone');
 const dangerZoneElement = document.createElement("div")
@@ -423,54 +423,54 @@ function respondRequest(user, value) {
     });
 }
 
-function fillInvoices(){
-  // Fetch the invoices data from the backend
-  fetch('/invoice/table')
-    .then(response => response.json())
-    .then(data => {
-      // Get the table body element
-      const tableBody = document.querySelector('#invoices-table tbody');
+// function fillInvoices(){
+//   // Fetch the invoices data from the backend
+//   fetch('/invoice/table')
+//     .then(response => response.json())
+//     .then(data => {
+//       // Get the table body element
+//       const tableBody = document.querySelector('#invoices-table tbody');
 
-      // Loop through the data and populate the table rows
-      data.forEach(invoice => {
-        invoice.upload_date= new Date(invoice.upload_date);
-        const options = { timeZone: 'Asia/Singapore' };
-        invoice.upload_date = (invoice.upload_date).toLocaleDateString('en-SG', options);
+//       // Loop through the data and populate the table rows
+//       data.forEach(invoice => {
+//         invoice.upload_date= new Date(invoice.upload_date);
+//         const options = { timeZone: 'Asia/Singapore' };
+//         invoice.upload_date = (invoice.upload_date).toLocaleDateString('en-SG', options);
 
-        // var deleteIcon = `
-        // <svg class="ic_delete"  xmlns="http://www.w3.org/2000/svg" value="${invoice.path}" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-        //     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-        //   </svg>`;
+//         // var deleteIcon = `
+//         // <svg class="ic_delete"  xmlns="http://www.w3.org/2000/svg" value="${invoice.path}" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+//         //     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+//         //   </svg>`;
         
-          var previewIcon =  `
-            <svg xmlns="http://www.w3.org/2000/svg" onclick="openImage(this.getAttribute('value'))" value='${invoice.path}' width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-              <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-              <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-            </svg>`
+//           var previewIcon =  `
+//             <svg xmlns="http://www.w3.org/2000/svg" onclick="openImage(this.getAttribute('value'))" value='${invoice.path}' width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+//               <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+//               <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+//             </svg>`
      
-          var exportIcon = `
-              <svg class="ic_export"  onclick="openText(this.getAttribute('value'))" xmlns="http://www.w3.org/2000/svg" value="${invoice.invoiceid}" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-              </svg>`;
+//           var exportIcon = `
+//               <svg class="ic_export"  onclick="openText(this.getAttribute('value'))" xmlns="http://www.w3.org/2000/svg" value="${invoice.invoiceid}" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+//                 <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+//                 <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+//               </svg>`;
       
-          // // render each row of data
-          tableBody.innerHTML += `
-            <tr>
-              <td>${invoice.invoiceid}</td>
-              <td>${invoice.invoice_name}</td>
-              <td>${invoice.upload_date}</td>
-              <td>${invoice.total}</td>
-              <td>${previewIcon}  ${exportIcon}</td>
-            </tr>`;
+//           // // render each row of data
+//           tableBody.innerHTML += `
+//             <tr>
+//               <td>${invoice.invoiceid}</td>
+//               <td>${invoice.invoice_name}</td>
+//               <td>${invoice.upload_date}</td>
+//               <td>${invoice.total}</td>
+//               <td>${previewIcon}  ${exportIcon}</td>
+//             </tr>`;
 
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching invoices data:', error);
-    });
+//       });
+//     })
+//     .catch(error => {
+//       console.error('Error fetching invoices data:', error);
+//     });
 
-}
+// }
 
 function openImage(value){
 window.open('http://127.0.0.1:8080/img-db/'+value);
