7469055c710b7b4a164e493c509bbb98763444ed
ChewonHan <about0322@naver.com>
2023-08-01 00:37:52 +0800

    added functions for checking wrong inputs

--- public/js/InvoiceEditPage.js
+++ public/js/InvoiceEditPage.js
@@ -44,31 +44,63 @@ document.getElementById("btn_edit_update_container").addEventListener("click", f
     const inp_comp_name = document.getElementById("inp_comp_name").value;
     const inp_issue_date = document.getElementById("inp_issue_date").value;
     const inp_total_amount = document.getElementById("inp_total_amount").value; 
+    const inp_phone_no = document.getElementById("inp_phone_no").value; 
   
     // Perform the necessary actions with the input values
     console.log(inp_comp_name);
     console.log(inp_issue_date);
     console.log(inp_total_amount);
+    console.log(inp_phone_no);
+
+    // Validate date format (dd/mm/yyyy)
+    const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!dateRegex.test(inp_issue_date)) {
+        document.getElementById("error_msg").textContent = "Invalid date format (dd/mm/yyyy)";
+        return;
+    }
+    else{
+        document.getElementById("error_msg").textContent = ""
+    }
+
+    // Validate amount as float
+    const amountRegex = /^\d+(\.\d{1,2})?$/;
+    if (!amountRegex.test(inp_total_amount)) {
+        document.getElementById("error_msg").textContent = "Invalid amount format (e.g., 123.45)";
+        return;
+    }
+    else{
+        document.getElementById("error_msg").textContent = ""
+    }
+
+    // Validate phone number format (contains only numbers)
+    const phoneNumberRegex = /^\d+$/;
+    if (!phoneNumberRegex.test(inp_phone_no)) {
+        document.getElementById("error_msg").textContent = "Invalid phone number format (should contain only numbers)";
+        return;
+    }
+    else{
+        document.getElementById("error_msg").textContent = ""
+    }
   
-    // You can use the input values for further processing, like updating data on the server, etc.
-    fetch('/update_data', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        input1: input1Value,
-        input2: input2Value
-      })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Server response:', data);
-      // You can update the UI or perform other actions based on the server response.
-    })
-    .catch(error => {
-      console.error('Error updating data:', error);
-      // Handle the error appropriately.
-    });
+    // // You can use the input values for further processing, like updating data on the server, etc.
+    // fetch('/update_data', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     input1: input1Value,
+    //     input2: input2Value
+    //   })
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   console.log('Server response:', data);
+    //   // You can update the UI or perform other actions based on the server response.
+    // })
+    // .catch(error => {
+    //   console.error('Error updating data:', error);
+    //   // Handle the error appropriately.
+    // });
   });
   
