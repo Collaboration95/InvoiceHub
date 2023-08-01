8011404b762fcb45c8395a3a73eee4d2d1e3e9d9
rpriyaj <radhipriya81@gmail.com>
2023-08-02 00:13:11 +0800

    Added more test cases for convertToCSV function

--- test/convertToCSV.test.js
+++ test/convertToCSV.test.js
@@ -38,4 +38,62 @@ describe('convertToCSV', () => {
     expect(csvContent).toEqual(expectedCSVContent);
   });
 
+  it('should handle missing or empty fields in detected texts', () => {
+    const detectedTexts = {
+      extractedDetails: {
+        // array is empty for Name
+        Name: [], 
+        Telephone: ['123456789', '987654321'],
+        Total: ['100', '200'],
+        //array is empty for IssuedDate
+        IssuedDate: [], 
+      },
+      table_data: [['Row 1'], ['Row 2']],
+    };
+  
+    const expectedCSVContent =
+        '"Name","Telephone","Total","IssuedDate","Table Data"\n"","123456789, 987654321","100, 200","","Row 1\nRow 2"';
+  
+    const csvContent = convertToCSV(detectedTexts);
+    expect(csvContent).toEqual(expectedCSVContent);
+  });
+
+
+  it('should handle missing or empty table_data', () => {
+    const detectedTexts = {
+      extractedDetails: {
+        Name: ['Jane', 'Tan'],
+        Telephone: ['123456789', '987654321'],
+        Total: ['100', '200'],
+        IssuedDate: ['2023-08-01', '2023-08-02'],
+      },
+      table_data: [],
+    };
+
+    const expectedCSVContent =
+      '"Name","Telephone","Total","IssuedDate","Table Data"\n"Jane, Tan","123456789, 987654321","100, 200","2023-08-01, 2023-08-02",""';
+
+    const csvContent = convertToCSV(detectedTexts);
+    expect(csvContent).toEqual(expectedCSVContent);
+  });
+
+  it('should handle multiple rows in table_data', () => {
+    const detectedTexts = {
+      extractedDetails: {
+        Name: ['Jane', 'Tan'],
+        Telephone: ['123456789', '987654321'],
+        Total: ['100', '200'],
+        IssuedDate: ['2023-08-01', '2023-08-02'],
+      },
+      table_data: [['Row 1'], ['Row 2'], ['Row 3']],
+    };
+
+    const expectedCSVContent =
+      '"Name","Telephone","Total","IssuedDate","Table Data"\n"Jane, Tan","123456789, 987654321","100, 200","2023-08-01, 2023-08-02","Row 1\nRow 2\nRow 3"';
+
+    const csvContent = convertToCSV(detectedTexts);
+    expect(csvContent).toEqual(expectedCSVContent);
+  });
+
+
 });
