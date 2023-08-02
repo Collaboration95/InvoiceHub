f2c89dc7c5b44772de679ffb9ce5407c9cd8968b
rummy0302 <bharathi.ramita@gmail.com>
2023-08-02 22:41:00 +0800

    Expense Graph Test 2 works

--- test/expense.test.js
+++ test/expense.test.js
@@ -34,5 +34,34 @@ describe('fetchData', () => {
     });
   });
 
- 
+  it('fetches and processes data correctly with no rows returned', async () => {
+    const fakeRows = [];
+
+    const mockExecute = jest.fn().mockResolvedValue([fakeRows]);
+    const mockConnection = {
+      execute: mockExecute,
+      end: jest.fn(),
+    };
+    jest.spyOn(require('mysql2/promise'), 'createConnection').mockResolvedValue(mockConnection);
+
+    const result = await fetchData();
+
+    expect(mockExecute).toHaveBeenCalledWith(expect.any(String));
+    expect(mockConnection.end).toHaveBeenCalled();
+
+    expect(result).toEqual({
+      labels: [],
+      datasets: [
+        {
+          label: 'Total Expense Per Month',
+          data: [],
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+        },
+      ],
+    });
+  });
+
+
 });
