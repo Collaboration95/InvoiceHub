930cf171ce67626d052ddfd9f9a719219123c99b
rummy0302 <bharathi.ramita@gmail.com>
2023-08-02 22:15:50 +0800

    Overdue Graph Test 2 works

--- test/overdue.test.js
+++ test/overdue.test.js
@@ -1,15 +1,13 @@
 const fetchData = require('../routes/overdue');
 
 describe('fetchData', () => {
-  it('fetches and processes data correctly', async () => {
-    // Mocking fake rows data
+  it('fetches and processes data correctly for different categories', async () => {
     const fakeRows = [
       { category: '<30 days', count: 5 },
       { category: '30-60 days', count: 10 },
       { category: '60-90 days', count: 15 },
     ];
 
-    // Mocking the MySQL connection and execute functions
     const mockExecute = jest.fn().mockResolvedValue([fakeRows]);
     const mockConnection = {
       execute: mockExecute,
@@ -19,7 +17,6 @@ describe('fetchData', () => {
 
     const result = await fetchData();
 
-    // Assertions
     expect(mockExecute).toHaveBeenCalledWith(expect.any(String));
     expect(mockConnection.end).toHaveBeenCalled();
 
@@ -36,4 +33,48 @@ describe('fetchData', () => {
       ],
     });
   });
+
+  it('fetches and processes data correctly with no rows returned', async () => {
+    const fakeRows = [
+      { category: '<30 days', count: 0 },
+      { category: '30-60 days', count: 0 },
+      { category: '60-90 days', count: 0 },
+    ];
+
+    const mockExecute = jest.fn().mockResolvedValue([fakeRows]);
+    const mockConnection = {
+      execute: mockExecute,
+      end: jest.fn(),
+    };
+    jest.spyOn(require('mysql2/promise'), 'createConnection').mockResolvedValue(mockConnection);
+
+    const result = await fetchData();
+
+    expect(mockExecute).toHaveBeenCalledWith(expect.any(String));
+    expect(mockConnection.end).toHaveBeenCalled();
+
+    expect(result).toEqual({
+      labels: ['<30 days', '30-60 days', '60-90 days'],
+      datasets: [
+        {
+          label: 'Amount Overdue',
+          data: [0,0,0],
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+        },
+      ],
+    });
+  });
+
+
 });
+
+
+
+
+
+
+
+
+
