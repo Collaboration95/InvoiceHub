7759f1a3212371e6aea87cf23003f2c28856073a
rummy0302 <bharathi.ramita@gmail.com>
2023-08-03 11:01:20 +0800

    Updated expense and overdue graph tests

--- test/overdue.test.js
+++ test/overdue.test.js
@@ -1,11 +1,12 @@
 const fetchData = require('../routes/overdue');
 
-describe('fetchData', () => {
+describe('fetchData function for Overdue Graph', () => {
   it('fetches and processes data correctly for different categories', async () => {
     const fakeRows = [
       { category: '<30 days', count: 5 },
       { category: '30-60 days', count: 10 },
       { category: '60-90 days', count: 15 },
+      { category: '>90 days', count: 5 },
     ];
 
     const mockExecute = jest.fn().mockResolvedValue([fakeRows]);
@@ -21,13 +22,23 @@ describe('fetchData', () => {
     expect(mockConnection.end).toHaveBeenCalled();
 
     expect(result).toEqual({
-      labels: ['<30 days', '30-60 days', '60-90 days'],
+      labels: ['<30 days', '30-60 days', '60-90 days', '>90 days'],
       datasets: [
         {
           label: 'Amount Overdue',
-          data: [5, 10, 15],
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)',
+          data: [5, 10, 15, 5],
+          backgroundColor: [
+            'rgba(255, 0, 0, 0.6)',
+            'rgba(192, 0, 0, 0.6)',
+            'rgba(130, 0, 0, 0.6)',
+            'rgba(81, 9, 9, 0.6)'
+          ],
+          borderColor: [
+            'rgba(255, 0, 0, 0.6)',
+            'rgba(192, 0, 0, 0.6)',
+            'rgba(130, 0, 0, 0.6)',
+            'rgba(81, 9, 9, 0.6)'
+          ],
           borderWidth: 1,
         },
       ],
@@ -39,6 +50,7 @@ describe('fetchData', () => {
       { category: '<30 days', count: 0 },
       { category: '30-60 days', count: 0 },
       { category: '60-90 days', count: 0 },
+      { category: '>90 days', count: 0 },
     ];
 
     const mockExecute = jest.fn().mockResolvedValue([fakeRows]);
@@ -54,48 +66,29 @@ describe('fetchData', () => {
     expect(mockConnection.end).toHaveBeenCalled();
 
     expect(result).toEqual({
-      labels: ['<30 days', '30-60 days', '60-90 days'],
+      labels: ['<30 days', '30-60 days', '60-90 days', '>90 days'],
       datasets: [
         {
           label: 'Amount Overdue',
-          data: [0,0,0],
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)',
+          data: [0, 0, 0, 0],
+          backgroundColor: [
+            'rgba(255, 0, 0, 0.6)',
+            'rgba(192, 0, 0, 0.6)',
+            'rgba(130, 0, 0, 0.6)',
+            'rgba(81, 9, 9, 0.6)'
+          ],
+          borderColor: [
+            'rgba(255, 0, 0, 0.6)',
+            'rgba(192, 0, 0, 0.6)',
+            'rgba(130, 0, 0, 0.6)',
+            'rgba(81, 9, 9, 0.6)'
+          ],
           borderWidth: 1,
         },
       ],
     });
   });
 
-  it('fetches and processes data correctly with no rows returned', async () => {
-    const fakeRows = [];
-  
-    const mockExecute = jest.fn().mockResolvedValue([fakeRows]);
-    const mockConnection = {
-      execute: mockExecute,
-      end: jest.fn(),
-    };
-    jest.spyOn(require('mysql2/promise'), 'createConnection').mockResolvedValue(mockConnection);
-  
-    const result = await fetchData();
-  
-    expect(mockExecute).toHaveBeenCalledWith(expect.any(String));
-    expect(mockConnection.end).toHaveBeenCalled();
-  
-    expect(result).toEqual({
-      labels: [],
-      datasets: [
-        {
-          label: 'Total Expense Per Month',
-          data: [],
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1,
-        },
-      ],
-    });
-  });
-  
 
   it('handles error when fetching data', async () => {
     const errorMessage = 'Database connection error';
