20c98154c9282bf203600cbfd62ee0d1d7153edd
Josiahfong <90824733+Josiahfong@users.noreply.github.com>
2023-08-09 17:09:32 +0800

    Update Notifications.test.js

--- test/Notifications.test.js
+++ test/Notifications.test.js
@@ -14,7 +14,7 @@ describe('should display notifications', () => {
 });
 //test to check if new notifications cabn be added
 describe('Add notification', ()=>{
-    if('should add notification', async ()=>{
+    it('should add notification', async ()=>{
         const response = await request(app);
         var dropdowncontent = document.querySelector('.dropdown-content');
         expect(dropdownItems.length).toBe(dropdownItems.length++);//checks that adding a notification increases the number of notifications by 1
