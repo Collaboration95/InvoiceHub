2452047c0ca67a4f6dd627313bed32bdac9bbc4b
Guruprasath <guruprasath3200@gmail.com>
2023-08-10 16:33:06 +0800

    Fixed Password bug

--- public/js/settings.js
+++ public/js/settings.js
@@ -183,7 +183,6 @@ function acceptFileInput(event) {
 function fillTable() {
   document.getElementById('id').textContent = localData.id;
   document.getElementById('user').textContent = localData.user;
-  document.getElementById('password').textContent = localData.password;
   document.getElementById('mode').textContent = localData.mode;
   document.getElementById('email').textContent = localData.Email;
 }
