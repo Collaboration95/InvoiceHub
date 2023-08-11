7f066d8e984e3e87316bc83590b10b327ba978be
Josiahfong <90824733+Josiahfong@users.noreply.github.com>
2023-08-11 14:08:23 +0800

    Update Notifications.test.js
    Updated test script

--- test/Notifications.test.js
+++ test/Notifications.test.js
@@ -1,24 +1,34 @@
+const {retrieveData_overdue} = require('../InvoiceHub-main/public/js/notif-new');
 const request = require('supertest');
 const express = require('express');
 const { describe } = require('node:test');
-const  app = express(); 
+const app = express();
 const router = express.Router();
-
 app.use(router);
-//test if information can be retrieved from database
-describe('should display notifications', () => {
-    it('should show notifications', async () => {
-        const response = await request(app).get('/account/all');//retrieve data from database
-        expect(response.body).toStrictEqual({});//check that the content for notifications from database can be retrieved 
-    });
+
+jest.mock('', () => {
+    return {
+        getData: jest.fn(() => {
+            try {
+                const sample_data = [
+                    { id: '12345', amount: '123.45', status: 'OVERDUE' }
+                ];
+
+                sample_data.forEach((invoice) => {
+                    invoice.statusColor = 'rgb(252, 183, 137)';
+                });
+                return sample_data;
+            } catch (error) {
+                console.error("Error fetching notifications data: ", error);
+            }
+        })
+    }
+
 });
-/*
-//test to check if new notifications cabn be added
-describe('Add notification', ()=>{
-    it('should add notification', async ()=>{
-        const response = await request(app);
-        var dropdowncontent = document.querySelector('.dropdown-content');
-        expect(dropdownItems.length).toBe(dropdownItems.length++);//checks that adding a notification increases the number of notifications by 1
-    });
-});
-*/
+describe("RetrieveData", ()=>{
+    it('should retrieve data from database', async()=>{
+        
+        const outcome = retrieveData_overdue();
+        expect(outcome);
+    })
+})
