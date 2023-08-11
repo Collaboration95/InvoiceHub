1768d1d5cad352ba4adf941e174fdebebd2c41d3
rpriyaj <radhipriya81@gmail.com>
2023-08-11 08:09:31 +0800

    Checked and Updated InvoicePage.js

--- public/js/InvoicePage.js
+++ public/js/InvoicePage.js
@@ -1,3 +1,4 @@
+//Final checked by Ramita and Radhi (11/08)
 var table = document.getElementById("invoice_table");
 
 // Function to fetch data fsrom the server
@@ -23,9 +24,6 @@ async function getData() {
 
 // Function to render the table with data
 function renderTable(data) {
-  // var unpaidCost =0;
-  // var overdueCost=0;
-  // var totalCost = 0;
   // set up the title of each column
   table.innerHTML = `
     <tr>
@@ -98,11 +96,7 @@ function renderTable(data) {
             <td>${previewIcon} ${editIcon} ${deleteIcon} ${exportIcon}</td>
           </tr>`;
   });
-  // totalCost = unpaidCost + overdueCost;
-  // console.log("cost",totalCost);
-  // document.getElementById("total_outstanding_cost").textContent = "S$ " + totalCost.toFixed(2);
-  // document.getElementById("overdue_cost").textContent = "S$ " + overdueCost.toFixed(2);
-  // document.getElementById("due_cost").textContent = "S$ " + unpaidCost.toFixed(2);
+
 }
 
 // Call getData() and renderTable() when the page loads
@@ -306,10 +300,7 @@ function sort_asc() {
   });
 }
 
-// function openImage(value) {
-//   // window.open('http://127.0.0.1:8080/' + value)
-//   window.open('http://127.0.0.1:8080/img-db/'+value);
-// }
+
 //  handles non existant image errors
 function openImage(value) {
   const imageUrl = 'http://localhost:8000/img-db/' + value;
